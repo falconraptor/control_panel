03c8ebf608d0876977008b5cb49dc8b57f19ce5a
falconraptor <falconraptor@users.noreply.github.com>
2019-01-05 01:10:38 -0800

    + logout page % redirect for auth

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def auth(f):  # TODO use as template? User form auth instead
     def wrapped(*args, **kwargs):
         request = kwargs.get('request', args[0] if args else Request())
         if 'auth' not in request.COOKIE:
-            return '', 303, {'Location': '/login/'}
+            return '', 303, {'Location': '/login/?next=' + request.PATH_INFO}
         return f(*args, **kwargs)
     wrapped.__name__ = f.__name__
     return wrapped
@@ -16,16 +16,23 @@ def auth(f):  # TODO use as template? User form auth instead
 
 @route(methods=['GET', 'POST'])
 def login(request):
-    if request.method == 'POST':
-        if pam().authenticate(request.GET['username'], request.GET['password']):
-            request.set_session('auth', request.GET['username'])
+    if request.REQUEST_METHOD == 'POST':
+        if pam().authenticate(request.POST['username'], request.POST['password']):
+            request.set_session('auth', '', path='/', http_only=True, secure=True)
+            return '', 303, {'Location': request.GET.get('next', '/')}
     return serve('html/login.html')
 
 
-@route()
+@route(methods=['GET'])
 @auth
-def login(request):
+def index(request):
     return serve('html/index.html')
+
+
+@route(methods=['GET'])
+def logout(request):
+    request.set_cookie('auth', value='', max_age=1, path='/', http_only=True, secure=True)
+    return '', 303, {'Location': '/login/'}
 
 
 if __name__ == '__main__':
